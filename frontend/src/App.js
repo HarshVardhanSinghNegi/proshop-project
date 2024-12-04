6f0a1c66cce5f366e7c8a644cf4fecd83c3750cc
HarshVardhanSinghNegi <monkeydloophie@gmail.com>
2024-12-04 10:49:45 +0530

    Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+// import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome Pro Shopper!!</h1>
-      
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
